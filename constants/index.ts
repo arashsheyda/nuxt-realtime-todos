8e276cc8a0bb131baf1ce9ec6c845e356475fe1a
Arash <arashi.sheyda@gmail.com>
2024-06-05 19:32:50 -0600

    chore: add links

--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,26 @@ export const socials = [
   { icon: 'i-simple-icons-twitter', to: 'https://twitter.com/arash_sheyda' },
   { icon: 'i-simple-icons-github', to: 'https://github.com/arashsheyda/nuxt-realtime-todos' },
 ]
+
+export const links = [
+  {
+    label: 'Slides',
+    icon: 'i-ph-monitor-play',
+    to: 'https://talks.arashsheyda.me/2024/real-time-todo',
+    target: '_blank',
+  },
+  {
+    label: 'Starter Template',
+    icon: 'i-ph-browser-duotone',
+    to: 'https://github.com/arashsheyda/nuxt-realtime-todos-starter',
+    color: 'blue',
+    target: '_blank',
+  },
+  {
+    label: 'Complete Repo',
+    icon: 'i-simple-icons-github',
+    to: 'https://github.com/arashsheyda/nuxt-realtime-todos',
+    color: 'black',
+    target: '_blank',
+  },
+]
